6cf997d83d947aa0e830d7ae8483a48e9535c9dc
E.Demidovskaia <violet7693@gmail.com>
2022-07-15 15:27:39 +0300

    feat: add getStaticProps to shop

--- pages/to-buy.jsx
+++ pages/to-buy.jsx
@@ -2,32 +2,35 @@
 import MainLayout from "../components/MainLayout"
 import Tasks from "../components/Tasks"
 
-/* CONST */
-const subtitle = "Shop list"
-const tasks = [
-    {
-        id: 1,
-        text: "Лук",
-        isCompleted: false,
-    },
-    {
-        id: 2,
-        text: "Картошка",
-        isCompleted: false,
-    },
-    {
-        id: 3,
-        text: "Хлеб",
-        isCompleted: false,
-    },
-]
-
-const Shop = () => {
-    return (
-        <MainLayout subtitle={subtitle}>
-            <Tasks defaultList={tasks} storageAddition="shop" />
-        </MainLayout>
-    )
+export function getStaticProps() {
+    return {
+        props: {
+            tasks: [
+                {
+                    id: 1,
+                    text: "Лук",
+                    isCompleted: false,
+                },
+                {
+                    id: 2,
+                    text: "Картошка",
+                    isCompleted: false,
+                },
+                {
+                    id: 3,
+                    text: "Хлеб",
+                    isCompleted: false,
+                },
+            ],
+            subtitle: "Shop list",
+        },
+    }
 }
+
+const Shop = ({ tasks, subtitle }) => (
+    <MainLayout subtitle={subtitle}>
+        <Tasks defaultList={tasks} storageAddition="shop" />
+    </MainLayout>
+)
 
 export default Shop
